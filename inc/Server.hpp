996ba700701890f2e9b04b9741c5795a541cd510
Hicham Ouabella <houabell@e2r7p13.1337.ma>
2026-03-20 22:11:56 +0000

    ...............

---
+++ inc/Server.hpp
@@ -0,0 +1,124 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include "ConfigLoader.hpp"
+#include <vector>
+#include <string>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <netdb.h>
+#include <fcntl.h>
+#include <cerrno>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/epoll.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+// state machine (all the states that we gonna go through in each connection)
+
+enum State {
+	READ_REQUEST_LINE,
+	READ_REQUEST_HEADER,
+	READ_BODY,
+	PROCESS_REQUEST,
+	WRITE_RESPONSE,
+	CLOSED
+};
+
+class	Client {
+	private:
+		int		fd;
+		int		listen_fd;
+		std::string	recv_buf;
+		std::string	send_buf;
+		State		state;
+
+		// parsed request (the data came from the client :browser)
+		std::string	method;
+		std::string	path;
+		std::string	version;
+		std::map<std::string, std::string> header;
+		std::string	body;
+		size_t		content_length;
+		int		error_code;
+
+		// Response streaming
+		size_t	file_size;
+		size_t	bytes_sent;
+		bool	header_sent;
+		bool	keep_alive;
+
+	public:
+		std::ifstream	file_stream;
+		
+		Client();
+		~Client();
+
+		// for re-use the same connection (keep-alive)
+		void	reset();
+
+		//connection identity
+		int	getFd() const;
+		int	getListenFd() const;
+		void	setListenFd(int lfd);
+
+		// state machine
+		std::string&		recvBuf();
+		std::string&		sendBuf();
+		void			setState(State s);
+		State			getState() const;
+		bool			requestComplete() const;
+
+		// request setters
+		void	setMethod(std::string m);
+		void	setPath(std::string p);
+		void	setVersion(std::string v);
+		void	setHeader(std::string key, std::string value);
+		void	setBody(const std::string& b);
+		void	setContentLength(size_t cl);
+		void	setErrorCode(int code);
+		void	setKeepAlive(bool ka);
+
+		// request getters
+		std::string				getMethod()	const;
+		std::string				getPath()	const;
+		std::string				getVersion()	const;
+		std::map<std::string, std::string>	getHeader()	const;
+		std::string				getBody()	const;
+		size_t					getContentLength() const;
+		int					getErrorCode()	const;
+		bool					isKeepAlive()	const;
+
+		// response file streaming
+		void		openFile(const std::string& path);
+		std::streamsize readFile(char * buf, std::size_t size);
+		void		setFileSize(size_t fs);
+		void		setBytesSent(size_t n);
+		void		setHeaderSent(bool hs);
+		bool		headerSent();
+		size_t		getBytesSent() const;
+		size_t		getFileSize() const;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
